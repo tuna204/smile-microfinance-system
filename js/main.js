07fae032330e092feadc2a3f89f8b8cb7da6ea56
tuna204 <85708853+tuna204@users.noreply.github.com>
2026-09-05 06:29:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,3 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.style.background = "red";
+});
